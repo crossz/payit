ee80062086be3149a3a8ca89c739da5aa146a113
Matrix <caiex@caiex-oltp2>
2015-07-02 20:00:38 +0800

    增加计算资金使用度统计数据功能

--- py_hdfsReader.py
+++ py_hdfsReader.py
@@ -100,34 +100,88 @@ def is_contains_key(key, set):
         
     return False
 
+def update_min_position():
+    cu_r = redis.Redis(host=ECS_ip, port=6379, db = 0)
+    keys = cu_r.keys('*minPosition')
+    total = 0
+    for key in keys:
+        if '(' in key:
+            aliveInv = cu_r.hgetall(key.replace('minPosition', 'aliveInvestment'))
+            if len(aliveInv) == 3 ** aliveInv.values()[0]:
+                keys = aliveInv.keys()
+                min = 9999999
+                
+                for key in keys:
+                    position = float(cu_r.hget(key.replace('minPosition', 'position'), key))
+                    if position < min:
+                        min = position
+                        
+                cu_r.set(key, min)
+                total = total + min
+            
+        else: 
+            get = cu_r.get(key)
+            total = total + get
+    
+    cu_r.set('riskInvestment', total)
+
 
 def hdfs_reduce_comb(args):
     
     cu_r = redis.Redis(host=ECS_ip, port=6379, db = 0)
     
+    #单关部分
     result = get_match_key(args)
     print(result)
+    
+    for i in range(len(result) - 1):
+        totalPrice = cu_r.get(args[0] + args[2 * i + 1] + 'totalPrice' + args[2 * i + 2])
+        totalInvest = cu_r.get(args[0] + args[2 * i + 1] + 'totalInvest')
+        cu_r.incrbyfloat('profiting', float(totalPrice))
+        cu_r.incrbyfloat('TotalAliveInvestment', -float(totalInvest))
+        cu_r.delete(args[0] + args[2 * i + 1] + 'minPosition')
+    
+    
     code = args[0]
     tmp = '(allUp*' + code + '*aliveInvestment'
-    keys = cu_r.keys('(allUp*' + code + '*aliveInvestment')
-    print(keys)
-    for key in keys:
-        hkeys = cu_r.hkeys(key)
+    hkeys = cu_r.keys('(allUp*' + code + '*aliveInvestment')
+    print(hkeys)
+    for hkey in hkeys:
+        keys = cu_r.hkeys(hkey)
         
-        for hkey in hkeys:
-            print(hkey)
+        for key in keys:
+            print(key)
             
             pipe = cu_r.pipeline(True, None)
             while 1:
                 try:
-                    pipe.watch(key)
-                    value = pipe.hget(key, hkey)
-                    if not is_contains_key(hkey, result):
-                        pipe.hset(key, hkey, -1)
-                        print('key:%s with hkey:%s has been deleted' % (key, hkey))
-                    else:
-                        pipe.hincrby(key, hkey, -1)
-                        print('key:%s with hkey:%s has been decreased by 1' % (key, hkey))
+                    pipe.watch(hkey)
+                    value = pipe.hget(hkey, key)
+                    if is_contains_key(key, result) and value != -1: #中奖了
+                        result = pipe.hincrby(key, hkey, -1)
+                        if result == 0:
+                            totalInvestment = pipe.hget(hkey.replace('aliveI', 'i'), 'totalInvestment')
+                            pipe.incrbyfloat('TotalAliveInvestment', -float(totalInvestment))
+                            pipe.delete(hkey.replace('aliveInvestment', 'minPosition'))
+#                             get = pool_redis.get('aliveInvestReduce', 0)
+#                             pool_redis['aliveInvestReduce'] = get + totalInvestment
+                        
+                        print('hkey:%s with key:%s has been decreased by 1' % (hkey, key))
+                    else: #没中奖
+                        pipe.hset(hkey, key, -1)
+                        values = pipe.hgetall(hkey).values()
+                        flag = True
+                        for value in values:
+                            if value == -1 :
+                                continue
+                            flag = False
+                        if flag:
+                            totalInvestment = pipe.hget(hkey.replace('aliveI', 'i'), 'totalInvestment')
+                            pipe.incrbyfloat('TotalAliveInvestment', -float(totalInvestment))
+                            pipe.delete(hkey.replace('aliveInvestment', 'minPosition'))
+#                             get = pool_redis.get('aliveInvestReduce', 0)
+#                             pool_redis['aliveInvestReduce'] = get + totalInvestment
+                        print('hkey:%s with key:%s has been set to -1' % (hkey, key))
                     exec_value = pipe.execute()
                     if len(exec_value) == 0:
                         break
@@ -161,5 +215,6 @@ if __name__ == "__main__":
     print pool_redis
     hdfs_reduce_inv(pool_redis)
     hdfs_reduce_comb(args[1::])
+    update_min_position()
     #pr.hdfs_reduce_comb(pool_redis)
     #pr.hdfs_rmdir()
